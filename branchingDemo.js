60307d303ead9da84ec49da348158f7f30185565
priyagokhale1 <75327119+priyagokhale1@users.noreply.github.com>
2022-09-09 12:39:50 -0400

    Update branchingDemo.js
    added functions to get the day and year
    reformatted the output

--- branchingDemo.js
+++ branchingDemo.js
@@ -9,6 +9,14 @@ const months = ["January", "February", "March", "April",
 const d = new Date();
 
 // getMonth() returns the month as an integer (0 - 11). Jan = 0, Dec = 11
-let month = months[d.getMonth()]
+// getDay() returns the day as an integer (1 - 31)
+// getFullYear() returns the year as a 4 digit number
 
-console.log("The date is: " + d);
+let month = months[d.getMonth()]
+let day = d.getDate();
+let year = d.getFullYear();
+
+let currentDate = "The current date is " + month + " " + day + ", " + year;
+
+console.log(d)
+console.log(currentDate);
